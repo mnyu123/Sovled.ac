6bb47b0cac20f9f597a3f1f590fff32a8717ec64
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2024-07-28 12:55:59 +0900

    1271번 풀이

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        // 1271번 엄청난 부자2
+//        Scanner scanner = new Scanner(System.in);
+//
+//        BigInteger AllMoney = scanner.nextBigInteger();
+//        BigInteger life = scanner.nextBigInteger();
+//
+//        System.out.println(AllMoney.divide(life));
+//        System.out.println(AllMoney.remainder(life));
+
+    }
+}
