c0b5ed18530e72f2994bc6e80111a7c5a25e1b32
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2024-07-28 13:12:48 +0900

    2475번 풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,5 +12,27 @@ public class Main {
 //        System.out.println(AllMoney.divide(life));
 //        System.out.println(AllMoney.remainder(life));
 
+        // 2475 검증수
+        Scanner scanner = new Scanner(System.in);
+
+        int total = 0;
+
+        for (int i = 0; i < 5; i++) {
+
+            int number = scanner.nextInt();
+
+            int test = number * number; // 제곱시킴
+            total = total + test; // 그거 토탈에 저장
+
+            System.out.println(i + "번째 제곱합한 수:" + total);
+
+
+        }
+
+        int sovle = total % 10;
+
+        System.out.println("이게 정답일듯: " + sovle);
+
+
     }
 }
