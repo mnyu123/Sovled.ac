65dcd2bd0e0b026b0e6acf9084ce81d275f48ed0
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2024-07-30 21:50:29 +0900

    2238번 풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,25 +13,38 @@ public class Main {
 //        System.out.println(AllMoney.remainder(life));
 
         // 2475 검증수
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int total = 0;
+//
+//        for (int i = 0; i < 5; i++) {
+//
+//            int number = scanner.nextInt();
+//
+//            int test = number * number; // 제곱시킴
+//            total = total + test; // 그거 토탈에 저장
+//
+//            System.out.println(i + "번째 제곱합한 수:" + total);
+//
+//
+//        }
+//
+//        int sovle = total % 10;
+//
+//        System.out.println("이게 정답일듯: " + sovle);
+
+
+
+
         Scanner scanner = new Scanner(System.in);
 
-        int total = 0;
+        BigInteger a =  scanner.nextBigInteger();
+        BigInteger b =  scanner.nextBigInteger();
 
-        for (int i = 0; i < 5; i++) {
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
 
-            int number = scanner.nextInt();
-
-            int test = number * number; // 제곱시킴
-            total = total + test; // 그거 토탈에 저장
-
-            System.out.println(i + "번째 제곱합한 수:" + total);
-
-
-        }
-
-        int sovle = total % 10;
-
-        System.out.println("이게 정답일듯: " + sovle);
 
 
     }
