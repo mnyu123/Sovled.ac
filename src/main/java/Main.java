9800d0661edb560b4a9aa3f9a1cc9853e5600bf0
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2024-08-01 20:11:36 +0900

    2420번 풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        
+        // 2420 사파리월드
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        //n과 m 입력받기
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+        System.out.print(Math.abs(n-m));  //n - m을 절대값을 한 값을 출력하면 된다
+        
+        
 //        // 1271번 엄청난 부자2
 //        Scanner scanner = new Scanner(System.in);
 //
@@ -36,14 +50,16 @@ public class Main {
 
 
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        BigInteger a =  scanner.nextBigInteger();
+//        BigInteger b =  scanner.nextBigInteger();
+//
+//        System.out.println(a.add(b));
+//        System.out.println(a.subtract(b));
+//        System.out.println(a.multiply(b));
 
-        BigInteger a =  scanner.nextBigInteger();
-        BigInteger b =  scanner.nextBigInteger();
-
-        System.out.println(a.add(b));
-        System.out.println(a.subtract(b));
-        System.out.println(a.multiply(b));
+        
 
 
 
