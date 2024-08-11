01618f60e54a354055dd4bb805a808e116165e82
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2024-08-11 14:06:03 +0900

    5339번 풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,25 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) {
 
-        // 5338번 마이크로소프트 로고
-        System.out.println("       _.-;;-._ ");
-        System.out.println("'-..-'|   ||   |");
-        System.out.println("'-..-'|_.-;;-._|");
-        System.out.println("'-..-'|   ||   |");
-        System.out.println("'-..-'|_.-''-._|");
+        // 5339번 콜센터
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+
+
+//        // 5338번 마이크로소프트 로고
+//        System.out.println("       _.-;;-._ ");
+//        System.out.println("'-..-'|   ||   |");
+//        System.out.println("'-..-'|_.-;;-._|");
+//        System.out.println("'-..-'|   ||   |");
+//        System.out.println("'-..-'|_.-''-._|");
 
 
 //        // 5337번
