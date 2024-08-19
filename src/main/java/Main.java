dd6adaab357121f07947dff99860b4a725857e25
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2024-08-19 20:59:58 +0900

    8545번 풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,39 @@
 import java.util.StringTokenizer;
 import java.util.Scanner;
+import java.math.BigInteger;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
-        // 8437 Julka
+        // 8545번
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger total = new BigInteger(bf.readLine());
-        BigInteger diff = new BigInteger(bf.readLine());
-        BigInteger two = new BigInteger("2");
-        //(1)
+        String input = bf.readLine(); // abc
 
-        BigInteger klaudia = (total.subtract(diff)).divide(two).add(diff);
-        //(2)
-        BigInteger natalia = (total.subtract(diff)).divide(two);
-        //(3)
-
-        System.out.println(klaudia);
-        System.out.println(natalia);
+        for (int i = input.length() - 1; i >= 0; i--) {
+            System.out.print(input.charAt(i));
+        }
 
     }
 }
 
+//// 8437 Julka
+//BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//BigInteger total = new BigInteger(bf.readLine());
+//BigInteger diff = new BigInteger(bf.readLine());
+//BigInteger two = new BigInteger("2");
+////(1)
+//
+//BigInteger klaudia = (total.subtract(diff)).divide(two).add(diff);
+////(2)
+//BigInteger natalia = (total.subtract(diff)).divide(two);
+////(3)
+//
+//        System.out.println(klaudia);
+//        System.out.println(natalia);
 //// 8393번
 //Scanner sc = new Scanner(System.in);
 //int input = sc.nextInt();
