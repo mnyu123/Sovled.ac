cb87ada5ce240db875371f9f0a73650b1d29d7ce
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2024-08-24 13:37:14 +0900

    10170번 풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
         // 여기에 작성
-        // 9654번 나부 함대 데이터
-        System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
-        System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
-        System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
-        System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
-        System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");
-        System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+        // 10170번 NFC West vs North
+        System.out.println("NFC West       W   L  T");
+        System.out.println("-----------------------");
+        System.out.println("Seattle        13  3  0");
+        System.out.println("San Francisco  12  4  0");
+        System.out.println("Arizona        10  6  0");
+        System.out.println("St. Louis      7   9  0");
+        System.out.println(); // 공백
+        System.out.println("NFC North      W   L  T");
+        System.out.println("-----------------------");
+        System.out.println("Green Bay      8   7  1");
+        System.out.println("Chicago        8   8  0");
+        System.out.println("Detroit        7   9  0");
+        System.out.println("Minnesota      5  10  1");
     }
+}
 
 
     // 여기에 작성
@@ -404,16 +412,6 @@ public class Main {
         // 여기에 작성
     ////public static void main(String[] args) {
         // 여기에 작성
-    ////}
-    ////// 여기에 작성
-    ////// 여기에 작성
-    /////*
-    //////System.out.println("dasdsa");
-*/
-
-    /*
-
-     */
 }
 
 /*
@@ -439,3 +437,24 @@ public class Main {
     //////// 여기에 작성
     //////System.out.println("테스트를해요");
 */
+
+/*
+    //// 여기에 작성
+    //// 9654번 나부 함대 데이터
+    //System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+    //System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
+    //System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
+    //System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
+    //System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");
+    //System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+    //// 여기에 작성
+    //// 여기에 작성
+    //////}
+    //////// 여기에 작성
+    //////// 여기에 작성
+    ///////*
+    ////////System.out.println("dasdsa");
+    //*/
+    ///*
+    //*/
+
