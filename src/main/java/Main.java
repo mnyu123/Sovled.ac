866e277905d92f35259590058df9feec76d7b528
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-01-04 15:26:51 +0900

    #11654 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,19 +12,9 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String A = bf.readLine(); // 입력값 받기
-
-        String[] arr = A.split(" "); // 공백 한 칸 기준으로 분리
-        Long a = Long.parseLong(arr[0]);
-        Long b = Long.parseLong(arr[1]);
-        Long c = Long.parseLong(arr[2]); // 아이씨 정민아 long타입으로 누가 숫자를 계산해
-
-        long result = add(a, b, c);
-        System.out.println(result);
-    }
-
-    private static long add(long s, long s1, long s2) {
-        return s + s1 + s2;
+        char word = bf.readLine().charAt(0); // 입력값 받기
+        int ascii = (int) word;
+        System.out.println(ascii);
     }
 }
 
