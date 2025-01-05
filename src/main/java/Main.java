15826df59e03d7640ba3eae13fb4377e8a562a8a
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-01-05 13:41:38 +0900

    #14681 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,32 +11,23 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("비와이");
-        
         // 여기에 작성
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String A = bf.readLine();
-        String arr[] = A.split(" ");
+        String X = bf.readLine();
+        String Y = bf.readLine();
+        int x = Integer.parseInt(X);
+        int y = Integer.parseInt(Y);
 
-        int N = Integer.parseInt(arr[0]); // 출발,종착역 빼고 정거장수 N  3역
-        int K = Integer.parseInt(arr[1]); // 출발역에서 탑승하는 사람수 k  2명
-
-        int total = 0;
-        total += K; // 처음에 탄사람들 미리 더함
-
-        for (int i = 0; i < N; i++) {
-            String a = bf.readLine();
-            String arr2[] = A.split(" ");
-            int ai = Integer.parseInt(arr2[0]); // i번째 정거장에서 탑승하는 사람수 (인원A)
-            int ab = Integer.parseInt(arr2[1]); // 하차하는 사람수 (인원B)
-            total += ai; // 타고
-            total -= ab; // 내리고
-
-            if (ai == 0) {
-                // 마지막 역은 아무도 안탐
-                // 버스기사 이름출력
-                // System.out.println("비와이");
-            }
+        if (x > 0 && y > 0) {
+            System.out.println("1");
+        } else if (x > 0 && y < 0) {
+            System.out.println("4");
+        } else if (x < 0 && y > 0) {
+            System.out.println("2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("3");
+        } else {
+            System.out.println("1");
         }
     }
 }
