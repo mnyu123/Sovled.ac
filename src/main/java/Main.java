64ede17f3be6c6d948a40bf2c0e1d309d479809e
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-01-05 14:27:16 +0900

    #14928 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,66 +12,71 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         // 여기에 작성
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String X = bf.readLine();
-        String Y = bf.readLine();
-        int x = Integer.parseInt(X);
-        int y = Integer.parseInt(Y);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine(); // 입력 숫자를 문자열로 읽기
 
-        if (x > 0 && y > 0) {
-            System.out.println("1");
-        } else if (x > 0 && y < 0) {
-            System.out.println("4");
-        } else if (x < 0 && y > 0) {
-            System.out.println("2");
-        } else if (x < 0 && y < 0) {
-            System.out.println("3");
-        } else {
-            System.out.println("1");
+        int divisor = 20000303; // 나눌 값
+        int remainder = 0;
+
+        // 문자열의 각 자리 숫자를 사용하여 나머지 계산
+        for (int i = 0; i < N.length(); i++) {
+            remainder = (remainder * 10 + (N.charAt(i) - '0')) % divisor;
         }
+
+        System.out.println(remainder); // 결과 출력
     }
 }
-
-
 /*
     //// 여기에 작성
     //BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    //String str = bf.readLine();
-    //System.out.println(str+"??!");
-*/
-
-/*
-    //// 여기에 작성
-    //BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    //int I = Integer.parseInt(bf.readLine()); // 테스트 케이스
-    //String A = bf.readLine();
-    //String arr[] = A.split(" ");
-    //int a = Integer.parseInt(arr[0]);
-    //int b = Integer.parseInt(arr[1]);
-    //int result = add(a, b);
-    //System.out.println(result);
+    //String X = bf.readLine();
+    //String Y = bf.readLine();
+    //int x = Integer.parseInt(X);
+    //int y = Integer.parseInt(Y);
+    //System.out.println("1");
+    //System.out.println("4");
+    //System.out.println("2");
+    //System.out.println("3");
+    //System.out.println("1");
     //}
-*/
-
-/*
-    //// 여기에 작성
-    //BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    //int I = Integer.parseInt(bf.readLine()); // 테스트 케이스
-    //String A = bf.readLine();
-    //String arr[] = A.split(" ");
-    //int a = Integer.parseInt(arr[0]);
-    //int b = Integer.parseInt(arr[1]);
-    //int result = add(a, b);
-    //System.out.println("Case " + "#" + (i + 1) + ": " + result);
     //}
-*/
-
-/*
-    //BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    //String A = bf.readLine();
-    //String arr[] = A.split(" "); // 공백 기준으로 나눔
-    //BigInteger a = new BigInteger(arr[0]);
-    //BigInteger b = new BigInteger(arr[1]);
-    //System.out.println(a.multiply(b));
-    //System.out.println("한국어 테스트");
-*/
+    //}
+    ///*
+    ////// 여기에 작성
+    ////BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    ////String str = bf.readLine();
+    ////System.out.println(str+"??!");
+    //*/
+///*
+////// 여기에 작성
+////BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+////int I = Integer.parseInt(bf.readLine()); // 테스트 케이스
+////String A = bf.readLine();
+////String arr[] = A.split(" ");
+////int a = Integer.parseInt(arr[0]);
+////int b = Integer.parseInt(arr[1]);
+////int result = add(a, b);
+////System.out.println(result);
+////}
+//*/
+///*
+////// 여기에 작성
+////BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+////int I = Integer.parseInt(bf.readLine()); // 테스트 케이스
+////String A = bf.readLine();
+////String arr[] = A.split(" ");
+////int a = Integer.parseInt(arr[0]);
+////int b = Integer.parseInt(arr[1]);
+////int result = add(a, b);
+////System.out.println("Case " + "#" + (i + 1) + ": " + result);
+////}
+//*/
+///*
+////BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+////String A = bf.readLine();
+////String arr[] = A.split(" "); // 공백 기준으로 나눔
+////BigInteger a = new BigInteger(arr[0]);
+////BigInteger b = new BigInteger(arr[1]);
+////System.out.println(a.multiply(b));
+////System.out.println("한국어 테스트");
+//*/
