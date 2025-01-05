4c86a460644a6f364be981aa9ca05fd328fe7ece
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-01-05 13:24:14 +0900

    #14581 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,14 +11,12 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 여기에 작성
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String A = bf.readLine();
-        String arr[] = A.split(" "); // 공백 기준으로 나눔
-
-        BigInteger a = new BigInteger(arr[0]);
-        BigInteger b = new BigInteger(arr[1]);
-
-        System.out.println(a.multiply(b));
+        String ID = bf.readLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + ID + "::fan:");
+        System.out.println(":fan::fan::fan:");
     }
 }
 
@@ -54,4 +52,14 @@ public class Main {
     //int result = add(a, b);
     //System.out.println("Case " + "#" + (i + 1) + ": " + result);
     //}
+*/
+
+/*
+    //BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    //String A = bf.readLine();
+    //String arr[] = A.split(" "); // 공백 기준으로 나눔
+    //BigInteger a = new BigInteger(arr[0]);
+    //BigInteger b = new BigInteger(arr[1]);
+    //System.out.println(a.multiply(b));
+    //System.out.println("한국어 테스트");
 */
