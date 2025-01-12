1d6f9f451673813ac1192402e08d780d75c91cc2
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-01-12 14:34:57 +0900

    #24078 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,28 +13,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // 여기에 작성
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int caseNumber = 0; // 테스트 케이스 번호
+        String A = bf.readLine();
+        int X = Integer.parseInt(A);
 
-        while (true) {
-            // 한 줄씩 읽기
-            String input = bf.readLine();
-            if (input == null || input.isEmpty()) {
-                break; // 입력이 없거나 비어 있으면 종료
-            }
-
-            // 공백 기준으로 분리
-            String[] parts = input.split(" ");
-            int N = Integer.parseInt(parts[0]); // 첫 번째 숫자는 배열 크기
-
-            // 종료 조건 확인
-            if (N == 0) {
-                break;
-            }
-
-            // 테스트 케이스 처리
-            caseNumber++;
-            System.out.println("Case " + caseNumber + ": Sorting... done!");
-        }
+        System.out.println(X % 21);
     }
 }
 
