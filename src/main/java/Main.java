2ebb99cde6e0f83b4fb4d0299e47be75187785d2
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-05 02:49:51 +0900

    #29163 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,12 +11,25 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        String[] arr = bf.readLine().split(" ");
 
-        int B = Integer.parseInt(bf.readLine());
-        int A = B * 10 / 110;
+        int evenCount = 0;
+        int oddCount = 0;
 
-        int result = B - A;
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(arr[i]);
+            if (number % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
 
-        System.out.println(result);
+        if (evenCount > oddCount) {
+            System.out.println("Happy");
+        } else {
+            System.out.println("Sad");
+        }
     }
 }
