4fd5b808b81d2da377ccc21af51c9b721ceb0b07
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-08 13:33:32 +0900

    #29751 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,21 +11,11 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
+        String[] arr = bf.readLine().split(" ");
 
-        String str = "WelcomeToSMUPC";
-        char[] arr = str.toCharArray();
+        float w = Float.parseFloat(arr[0]);
+        float h = Float.parseFloat(arr[1]);
 
-        if (N < 15) {
-            System.out.println(arr[N - 1]); // 1-based index 처리
-        } else if (N == 15) {
-            System.out.println(arr[0]); // 15번째는 "W"
-        } else {
-            if (N % 14 == 0) {
-                System.out.println(arr[13]); // 14번째("C")
-            } else {
-                System.out.println(arr[(N % 14) - 1]); // 나머지 처리
-            }
-        }
+        System.out.printf("%.1f", w * h / 2);
     }
 }
