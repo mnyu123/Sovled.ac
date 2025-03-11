2c5aa285a8db184756a6680f70cc558c1d99c8d9
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-11 20:46:18 +0900

    #30214 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,40 +12,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(bf.readLine());
+        String[] arr = bf.readLine().split(" ");
 
-        String A = "";
+        int s1 = Integer.parseInt(arr[0]); // 대회 전반부가 끝났을 때 해당 문제를 해결한 참가자의 수
+        int s2 = Integer.parseInt(arr[1]);
+        // 대회가 완전히 끝났을 때 해당 문제를 해결한 참가자의 수 s2
+        //(즉, s2는 s1을 포함한 전체 해결자 수이다.)
 
-        for (int i = 0; i < N; i++) {
-            A = bf.readLine();
-            System.out.println(info(A));
+        // 만약 전체 해결자(s2)의 절반 이상이 대회 전반부(s1)에 문제를 해결했다면, 이 문제는 쉬운 문제로 간주된다.
+        // 쉬운문제 기준 : s2값의 절반을 구하고 , s1이 그 값을 넘거나 같으면
+
+        if ((s2 + 1) / 2 <= s1) {
+            System.out.println("E");
+        } else {
+            System.out.println("H");
         }
-    }
-
-    public static String info(String A) {
-        switch (A) {
-            case "Algorithm":
-                A = "204";
-                break;
-            case "DataAnalysis":
-                A = "207";
-                break;
-            case "ArtificialIntelligence":
-                A = "302";
-                break;
-            case "CyberSecurity":
-                A = "B101";
-                break;
-            case "Network":
-                A = "303";
-                break;
-            case "Startup":
-                A = "501";
-                break;
-            case "TestStrategy":
-                A = "105";
-                break;
-        }
-        return A;
     }
 }
