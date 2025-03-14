d54eac48d1a3f49c5580609411e603f8cd206c39
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-14 18:33:45 +0900

    #30328 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,24 +11,8 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String sN = bf.readLine();
-        int N = Integer.parseInt(sN);
+        int n = Integer.parseInt(bf.readLine());
 
-        // 숫자에 7 포함되어있고
-        if (sN.contains("7")) {
-            if (N % 7 == 0) { // 숫자에 7 포함되어있고 + 7로 나누어 떨어지는 경우
-                System.out.println(3);
-            } else { // 숫자에 7 포함되어있고 + 7로 나누어 떨어지지 않는 경우
-                System.out.println(2);
-            }
-        }
-        // 숫자에 7이 포함안되어 있고
-        else {
-            if (N % 7 == 0) { // 숫자에 7 포함안되어 있고 + 7로 나누어 떨어지는 경우
-                System.out.println(1);
-            } else { // 숫자에 7 포함안되어 있고 + 7로 나누어 떨어지지 않는 경우
-                System.out.println(0);
-            }
-        }
+        System.out.println(n * 4000);
     }
 }
