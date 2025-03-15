76a90ae13a8f3b9b220e94e842352c40471e81b1
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-15 23:01:28 +0900

    #30664 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,8 +11,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
 
-        System.out.println(n * 4000);
+        while (true) {
+            String A = bf.readLine();
+
+            if (A.equals("0")) {
+                break;
+            }
+
+            BigInteger n = new BigInteger(A);
+            BigInteger div = BigInteger.valueOf(42);
+
+            if (n.mod(div).equals(BigInteger.ZERO)) {
+                System.out.println("PREMIADO");
+            } else {
+                System.out.println("TENTE NOVAMENTE");
+            }
+        }
     }
 }
