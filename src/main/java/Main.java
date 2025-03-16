371b715c7e0cdf48a0764bd26a79aec4e4e4a258
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-16 23:50:51 +0900

    #30676 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,22 +11,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int Ramda = Integer.parseInt(bf.readLine());
 
-        while (true) {
-            String A = bf.readLine();
-
-            if (A.equals("0")) {
-                break;
-            }
-
-            BigInteger n = new BigInteger(A);
-            BigInteger div = BigInteger.valueOf(42);
-
-            if (n.mod(div).equals(BigInteger.ZERO)) {
-                System.out.println("PREMIADO");
-            } else {
-                System.out.println("TENTE NOVAMENTE");
-            }
+        if (Ramda >= 380 && Ramda < 425) {
+            System.out.println("Violet");
+        } else if (Ramda >= 425 && Ramda < 450) {
+            System.out.println("Indigo");
+        } else if (Ramda >= 450 && Ramda < 495) {
+            System.out.println("Blue");
+        } else if (Ramda >= 495 && Ramda < 570) {
+            System.out.println("Green");
+        } else if (Ramda >= 570 && Ramda < 590) {
+            System.out.println("Yellow");
+        } else if (Ramda >= 590 && Ramda < 620) {
+            System.out.println("Orange");
+        } else if (Ramda >= 620 && Ramda <= 780) {
+            System.out.println("Red");
         }
     }
 }
