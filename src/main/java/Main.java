a59975b6e71270582b7f2bbbf607a538894f5c0d
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-19 22:10:49 +0900

    #2562 문제풀이 커밋(처음으로 실버풀어봄)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,36 +10,24 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 한 번만 선언
-        int count = 0; // 그룹 단어 개수 저장
-        int N = Integer.parseInt(bf.readLine()); // 단어 개수 입력
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[] arrs = new int[9];
 
-        for (int i = 0; i < N; i++) {
-            String word = bf.readLine(); // 단어 입력
-            if (check(word)) { // 그룹 단어인지 확인
-                count++;
+        for (int i = 0; i < arrs.length; i++) {
+            arrs[i] = Integer.parseInt(bf.readLine()); // 배열에 입력
+        }
+
+        int max = arrs[0];
+        int maxIndex = 0;
+
+        for (int j = 1; j < arrs.length; j++) {
+            if (arrs[j] > max) {
+                max = arrs[j];  // 최대값 업데이트
+                maxIndex = j;   // 최대값이 있는 인덱스 업데이트
             }
         }
 
-        System.out.println(count);
-    }
-
-    public static boolean check(String C) {
-        boolean[] alphabet = new boolean[26]; // 알파벳 등장 여부 체크
-        char prev = 0; // 이전 문자 저장
-
-        for (int i = 0; i < C.length(); i++) {
-            char now = C.charAt(i); // 현재 문자
-
-            // 이전 문자와 다르고, 이미 등장한 문자라면 그룹 단어가 아님
-            if (now != prev) {
-                if (alphabet[now - 'a']) { // 중복 확인
-                    return false; // 위에 if절이 true 면 return false를 줘서 count 안함 -> 왜? 그룹워드 아니라서
-                }
-                alphabet[now - 'a'] = true; // 등장 기록
-                prev = now; // 이전 문자 갱신
-            }
-        }
-        return true;
+        System.out.println(max);
+        System.out.println(maxIndex + 1); // 0부터 인덱스 세는게 아님 1부터 세야함(이번 문제에서만!)
     }
 }
