e0639e15bff6d46c6b2cdce98c33c97f58f17e74
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-21 18:52:08 +0900

    #2675 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,19 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " "); // 공백 기준으로 단어받음
+        int T = Integer.parseInt(bf.readLine());
 
-        System.out.println(st.countTokens());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine(), " "); // "3" , "ABC" 로 나뉨
+            int R = Integer.parseInt(st.nextToken()); // 문자열 반복할 횟수
+            String S = st.nextToken();
+
+            for (int j = 0; j < S.length(); j++) {  // 문자열 S의 각 문자에 대해
+                for (int k = 0; k < R; k++) {  // 해당 문자를 R번 반복 출력
+                    System.out.print(S.charAt(j));
+                }
+            }
+            System.out.println(); // 줄바꿈
+        }
     }
 }
