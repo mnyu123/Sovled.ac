2ed1d25d373d5b4a6cc15bbee3d8cd9be6664854
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-21 19:07:50 +0900

    #10809 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bf.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine(); // 단어 입력
+        int[] alphabet = new int[26];
 
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " "); // "3" , "ABC" 로 나뉨
-            int R = Integer.parseInt(st.nextToken()); // 문자열 반복할 횟수
-            String S = st.nextToken();
+        for (int i = 0; i < alphabet.length; i++) {
+            alphabet[i] = -1; // 처음에는 전부 -1로 초기화(아직 아무것도 안나왔으니까)
+        }
 
-            for (int j = 0; j < S.length(); j++) {  // 문자열 S의 각 문자에 대해
-                for (int k = 0; k < R; k++) {  // 해당 문자를 R번 반복 출력
-                    System.out.print(S.charAt(j));
-                }
+        for (int j = 0; j < S.length(); j++) {
+            char ch = S.charAt(j); // 왼쪽부터 찾음
+            int index = ch - 'a'; // 알파벳이 등장한 인덱스임
+            if (alphabet[index] == -1) {
+                alphabet[index] = j; // 알파벳 그 인덱스 위치에 현재 j위치 넣어서 저장
             }
-            System.out.println(); // 줄바꿈
+        }
+
+        for (int k = 0; k < alphabet.length; k++) {
+            System.out.print(alphabet[k] + " ");
         }
     }
 }
