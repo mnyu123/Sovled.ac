5241f58fa1e338d59c543a4fca6ecc3ab1261069
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-23 23:37:14 +0900

    #31450 문제풀이 커밋_오늘은 한문제만 품

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,29 +6,15 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        // 같은눈 3개 (6,6,6) 10000+(6)x1000원 상금 -> 16000원
-        // 같은눈 2개 (5,5,1) 1000+(5)x100원 상금 -> 1500원
-        // 모두 다름 (1,2,3) 3x100원 상금 -> 300원
-
         StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
 
-        if (a == b && b == c) {
-            System.out.println(10000 + (a * 1000));
-        } else if (a == b || b == c || a == c) {
-            int same;
-            if (a == b) same = a;
-            else if (b == c) same = b;
-            else same = a; // (a == c)
-            System.out.println(1000 + (same * 100));
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        if (M % K == 0) {
+            System.out.println("Yes");
         } else {
-            int max;
-            if (a > b && a > c) max = a;
-            else if (b > a && b > c) max = b;
-            else max = c;
-            System.out.println(max * 100);
+            System.out.println("No");
         }
     }
 }
