2cfd5fb389f9cd8138cd184e26283f09135b42ae
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-24 20:12:36 +0900

    #2577 문제풀이 커밋_일의자리 숫자 뽑아내기를 알아야한다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,26 +6,29 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        // 구매한 각 물건의 가격과 개수
-        // 구매한 물건들의 '총 금액'
-        // 구매한 물건의 가격과 개수로 계산한 총 금액 == 영수증 총 금액과 일치하는지?
+        // A,B,C 가 준다
+        // 1. A*B*C를 곱한결과에 0부터 9까지 숫자가 몇번 쓰였는지
+        // 계산결과가 17037300 라면 0이 3번,1 1번,3 두번,7 두번임
 
-        int X = Integer.parseInt(bf.readLine()); // 영수증 총 금액
-        int N = Integer.parseInt(bf.readLine()); // 영수증에 찍힌 구매한 물건의 종류 수
+        int A = Integer.parseInt(bf.readLine());
+        int B = Integer.parseInt(bf.readLine());
+        int C = Integer.parseInt(bf.readLine());
 
-        int buy = 0;
+        int mul = A * B * C;
+        check(mul);
+    }
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-            int bought = Integer.parseInt(st.nextToken());
-            int Count = Integer.parseInt(st.nextToken());
-            buy += bought * Count;
+    public static void check(int num) {
+        int[] count = new int[10];
+
+        while (num > 0) {
+            int number = num % 10; // ex 173 % 10 -> 3 나머지 -> 일의 자리 숫자임
+            count[number]++; // 일의 자리 숫자를 count 배열에 해당 인덱스에 넣어서 ++ -> 0 0 1 0 0 0 0 ... 이렇게 됨
+            num /= 10; // 이제 173 / 10 -> 17 -> 반복하면 두번째에 7 걸림 -> 반복
         }
 
-        if (X == buy) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
+        for (int i = 0; i < count.length; i++) {
+            System.out.println(count[i]);
         }
     }
 }
