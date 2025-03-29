b6433010df1833bc2df841794a641a68a312429b
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-29 22:11:30 +0900

    #32260 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,26 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
 
-        String[][] board = {{"11", "A B C D E F G H J L M"},
-                {"9", "A C E F G H I L M"},
-                {"9", "A C E F G H I L M"},
-                {"9", "A B C E F G H L M"},
-                {"8", "A C E F G H L M"},
-                {"8", "A C E F G H L M"},
-                {"8", "A C E F G H L M"},
-                {"8", "A C E F G H L M"},
-                {"8", "A C E F G H L M"},
-                {"8", "A B C F G H L M"}};
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-        // Output
-        System.out.println(board[N - 1][0]);
-        System.out.println(board[N - 1][1]);
+        add(A, B);
+    }
+
+    public static int add(int A, int B) {
+        return A + B;
     }
 }
