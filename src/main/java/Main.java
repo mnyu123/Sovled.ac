24d23399cb95c070a4955efd14edb38ea481a04a
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-03-29 22:07:16 +0900

    #32154 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,21 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
 
-        boolean[] number = new boolean[31];
+        String[][] board = {{"11", "A B C D E F G H J L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A B C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A B C F G H L M"}};
 
-        for (int i = 0; i < 28; i++) {
-            // 입력
-            int check = Integer.parseInt(bf.readLine());
-            number[check] = true; // 숫자 있으면 true
-        }
-        for (int i = 1; i <= 30; i++) {
-            if (!number[i]) { // false(NOT) -> true, 즉 아까 입력때 없었으면
-                System.out.println(i);
-            }
-        }
+        // Output
+        System.out.println(board[N - 1][0]);
+        System.out.println(board[N - 1][1]);
     }
 }
