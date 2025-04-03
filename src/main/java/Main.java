f62077e85b915dd17026186ad23c668de76ed639
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-04-03 17:26:45 +0900

    #32314 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,18 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int Sab = Integer.parseInt(bf.readLine());
-        int Fab = Integer.parseInt(bf.readLine());
+        int ampere = Integer.parseInt(bf.readLine()); // 트리의 암페어
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int watt = Integer.parseInt(st.nextToken());
+        int volt = Integer.parseInt(st.nextToken());
 
-        if (Sab > Fab) {
-            System.out.println("flight");
+        // 조건 트리 암페어(ampere)값 >= 계산한 암페어(check)값
+        int check = watt / volt;
+
+        if (check >= ampere) {
+            System.out.println("1");
         } else {
-            System.out.println("high speed rail");
+            System.out.println("0");
         }
     }
 }
