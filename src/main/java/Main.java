3347e9142f1294161b6efe139f96390bec4abf2f
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-04-08 18:55:06 +0900

    #33161 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int ampere = Integer.parseInt(bf.readLine()); // 트리의 암페어
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        int watt = Integer.parseInt(st.nextToken());
-        int volt = Integer.parseInt(st.nextToken());
-
-        // 조건 트리 암페어(ampere)값 >= 계산한 암페어(check)값
-        int check = watt / volt;
-
-        if (check >= ampere) {
-            System.out.println("1");
-        } else {
-            System.out.println("0");
-        }
+        int A = Integer.parseInt(bf.readLine());
+        System.out.println(A / 5);
     }
 }
