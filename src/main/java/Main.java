b2417b0f9e389c253af308031c9e3c1b801ed601
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-04-09 20:46:34 +0900

    #32326 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,11 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int A = Integer.parseInt(bf.readLine());
-        System.out.println(A / 5);
+
+        int susuiA = Integer.parseInt(bf.readLine());
+        int susuiB = Integer.parseInt(bf.readLine());
+        int susuiC = Integer.parseInt(bf.readLine());
+
+        System.out.println((3 * susuiA) + (4 * susuiB) + (5 * susuiC));
     }
 }
