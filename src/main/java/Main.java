47dda0c1873e28ed75f5117bd6f584249f5c623b
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-04-12 21:15:56 +0900

    #10808 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
 
-        int R1 = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
+        String S = bf.readLine();
+        int[] count = new int[26];
 
-        // R1+R2 / 2 평균이랑 R1을 기억하고 있으니까 R2를 유추하래요
-        System.out.println((S * 2) - R1);
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            count[c - 'a']++;
+        }
+
+        for (int j = 0; j < count.length; j++) {
+            System.out.print(count[j] + " ");
+        }
     }
 }
