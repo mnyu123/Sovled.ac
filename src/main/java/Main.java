a1169ccab21f16dc7b1454ae70eadffe3f82d6bb
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2025-04-15 23:47:54 +0900

    #10101 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int angle1 = Integer.parseInt(bf.readLine());
+        int angle2 = Integer.parseInt(bf.readLine());
+        int angle3 = Integer.parseInt(bf.readLine());
 
-        int[] prices = new int[5];
-        for (int i = 0; i < 5; i++) {
-            int price = Integer.parseInt(bf.readLine());
-            prices[i] = price;
+        int sum = angle1 + angle2 + angle3; // 각의 합
+
+        if (angle1 == 60 && angle2 == 60 && angle3 == 60) {
+            System.out.println("Equilateral");
+        } else if (sum == 180) {
+            if (angle1 == angle2 || angle2 == angle3 || angle1 == angle3) {
+                System.out.println("Isosceles");
+            } else {
+                System.out.println("Scalene");
+            }
+        } else {
+            System.out.println("Error");
         }
-        int minBurger = Math.min(prices[0], Math.min(prices[1], prices[2]));
-        int minDrink = Math.min(prices[3], prices[4]);
-
-        int setPrice = minBurger + minDrink - 50;
-
-        System.out.println(setPrice);
     }
 }
