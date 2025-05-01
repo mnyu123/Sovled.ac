cae018791f8ccfe90d02cd702e59f527ae9870f1
mnyu123 <whdudwo5582@gmail.com>
2025-05-01 21:29:37 +0900

    #2845 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-        System.out.println(n);
-
-        // for문이 하나라서 어떻게 수행해도 최고차항 계수가
-        System.out.println(1);
-        // 1
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int L = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int visit = L * P;
+        StringTokenizer st2 = new StringTokenizer(bf.readLine(), " ");
+        for (int i = 0; i < 5; i++) {
+            int visitor = Integer.parseInt(st2.nextToken());
+            System.out.print((visitor - visit) + " ");
+        }
     }
 }
