9a6421528effed7fe5e987e4abcbdb848f452588
mnyu123 <whdudwo5582@gmail.com>
2025-05-06 19:46:09 +0900

    #15439 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,8 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        int L = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-        int visit = L * P;
-        StringTokenizer st2 = new StringTokenizer(bf.readLine(), " ");
-        for (int i = 0; i < 5; i++) {
-            int visitor = Integer.parseInt(st2.nextToken());
-            System.out.print((visitor - visit) + " ");
-        }
+        int N = Integer.parseInt(bf.readLine());
+
+        System.out.println(N * (N - 1));
     }
 }
