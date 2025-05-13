471a1d55c16fba791a62a395de60dafb6125f980
mnyu123 <whdudwo5582@gmail.com>
2025-05-13 22:29:32 +0900

    #32929 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,18 +5,14 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
+        int X = Integer.parseInt(bf.readLine());
 
-        int year = 2024;
-        int month = 8;
-
-        int addMonths = (N - 1) * 7;
-        month += addMonths;
-
-        // month가 12를 넘으면 연도로 넘겨야 함
-        year += (month - 1) / 12;
-        month = (month - 1) % 12 + 1;
-
-        System.out.println(year + " " + month);
+        if (X % 3 == 1) {
+            System.out.println("U");
+        } else if (X % 3 == 2) {
+            System.out.println("O");
+        } else if (X % 3 == 0) {
+            System.out.println("S");
+        }
     }
 }
