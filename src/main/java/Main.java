100d0d44878be7c7fd58870997d21749c29d94d8
mnyu123 <whdudwo5582@gmail.com>
2025-05-14 20:38:49 +0900

    #32458 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,10 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int X = Integer.parseInt(bf.readLine());
+        String X = bf.readLine();
+//         int X  = Integer.parseInt(bf.readLine());
+        String[] parts = X.split("\\.");
 
-        if (X % 3 == 1) {
-            System.out.println("U");
-        } else if (X % 3 == 2) {
-            System.out.println("O");
-        } else if (X % 3 == 0) {
-            System.out.println("S");
-        }
+        System.out.println(parts[0]);
     }
 }
