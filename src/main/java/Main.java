1ab93ab54f1f6e72dd0b75da5ad51a5b0decf9ab
mnyu123 <whdudwo5582@gmail.com>
2025-05-18 20:08:11 +0900

    #33165 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,9 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            int problem = Integer.parseInt(bf.readLine());
+        int T = Integer.parseInt(bf.readLine());
+        int V = Integer.parseInt(bf.readLine());
 
-            if(problem % 2 == 1){
-                result++;
-            }
-        }
-        System.out.println(result);
+        System.out.println(T * V);
     }
 }
