e19f2c4698b4e149d964fd7cb9970126e3d7ee39
mnyu123 <whdudwo5582@gmail.com>
2025-05-22 21:19:47 +0900

    #1264 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,20 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int A = Integer.parseInt(bf.readLine());
-        int B = Integer.parseInt(bf.readLine());
+        String input;
 
-        System.out.println((A * 1000) + (B * 10000));
+        while (!(input = bf.readLine()).equals("#")) {
+            int count = 0;
+            input = input.toLowerCase(); // 대소문자 구분 없이 비교하기 위해 소문자로 변환
+
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+        }
     }
 }
