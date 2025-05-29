7155cf45b0fd9a681db69f3c043308430b00e877
mnyu123 <whdudwo5582@gmail.com>
2025-05-29 21:19:47 +0900

    #2083 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,22 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int result = 1;
 
-        for (int i = N; i > 0; i--) {
-            result = result * i;
+        while (true) {
+            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+            String name = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int mom = Integer.parseInt(st.nextToken());
+
+            if (name.equals("#") && age == 0 && mom == 0) {
+                break;
+            }
+
+            if (age > 17 || mom >= 80) {
+                System.out.println(name + " Senior");
+            } else {
+                System.out.println(name + " Junior");
+            }
         }
-        System.out.println(result);
-
     }
 }
