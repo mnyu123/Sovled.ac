f6bcf1307016ebe0581ec6e5390285f8f93bcc01
mnyu123 <whdudwo5582@gmail.com>
2025-05-31 12:51:13 +0900

    #5532 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
 
-        for (int i = 0; i < N; i++) {
-            String name = bf.readLine();
-            System.out.println(name.toLowerCase());
-        }
+        int L = Integer.parseInt(bf.readLine()); // 방학 일수
+        int A = Integer.parseInt(bf.readLine()); // 국어 총 페이지
+        int B = Integer.parseInt(bf.readLine()); // 수학 총 페이지
+        int C = Integer.parseInt(bf.readLine()); // 하루에 풀 수 있는 국어 페이지
+        int D = Integer.parseInt(bf.readLine()); // 하루에 풀 수 있는 수학 페이지
+
+        int koreanDays = (A + C - 1) / C;
+        int mathDays = (B + D - 1) / D;
+
+        int studyDays = Math.max(koreanDays, mathDays); // 더 많이 소모하는 일자로 빼면 남은 방학일자
+        System.out.println(L - studyDays);
     }
 }
