439281536520869dd01954837d7822f5800af3f8
mnyu123 <whdudwo5582@gmail.com>
2025-06-01 22:11:33 +0900

    #5358 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String line;
 
-        int L = Integer.parseInt(bf.readLine()); // 방학 일수
-        int A = Integer.parseInt(bf.readLine()); // 국어 총 페이지
-        int B = Integer.parseInt(bf.readLine()); // 수학 총 페이지
-        int C = Integer.parseInt(bf.readLine()); // 하루에 풀 수 있는 국어 페이지
-        int D = Integer.parseInt(bf.readLine()); // 하루에 풀 수 있는 수학 페이지
+        while ((line = bf.readLine()) != null) {
+            // 일단 저걸로 바꾸고
+            line = line.replace("i", "#");
+            line = line.replace("e", "%");
+            line = line.replace("I", "$");
+            line = line.replace("E", "&");
 
-        int koreanDays = (A + C - 1) / C;
-        int mathDays = (B + D - 1) / D;
+            // 겹치는 상황 지나간 다음에 바꾸기
+            line = line.replace("#", "e");
+            line = line.replace("%", "i");
+            line = line.replace("$", "E");
+            line = line.replace("&", "I");
 
-        int studyDays = Math.max(koreanDays, mathDays); // 더 많이 소모하는 일자로 빼면 남은 방학일자
-        System.out.println(L - studyDays);
+            System.out.println(line);
+        }
     }
 }
