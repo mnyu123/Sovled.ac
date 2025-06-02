77719bfeb5fa9bd36dafbb2c6e5c9840c968aa65
mnyu123 <whdudwo5582@gmail.com>
2025-06-02 22:53:58 +0900

    #4470 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,11 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String line;
+        int N = Integer.parseInt(bf.readLine());
 
-        while ((line = bf.readLine()) != null) {
-            // 일단 저걸로 바꾸고
-            line = line.replace("i", "#");
-            line = line.replace("e", "%");
-            line = line.replace("I", "$");
-            line = line.replace("E", "&");
-
-            // 겹치는 상황 지나간 다음에 바꾸기
-            line = line.replace("#", "e");
-            line = line.replace("%", "i");
-            line = line.replace("$", "E");
-            line = line.replace("&", "I");
-
-            System.out.println(line);
+        for (int i = 0; i < N; i++) {
+            String comment = bf.readLine();
+            System.out.println((i + 1) + ". " + comment);
         }
     }
 }
