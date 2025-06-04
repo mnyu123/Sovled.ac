2c078a6e73285417406cffcf719874d7363339fe
mnyu123 <whdudwo5582@gmail.com>
2025-06-04 23:08:29 +0900

    #5300 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,16 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
 
-        for (int i = 0; i < N; i++) {
-            String comment = bf.readLine();
-            System.out.println((i + 1) + ". " + comment);
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i + " ");
+            if (i % 6 == 0) {
+                System.out.print("Go! ");
+            }
+        }
+
+        // 마지막 그룹에 6명이 안 됐을 경우에도 Go! 출력
+        if (N % 6 != 0) {
+            System.out.print("Go!");
         }
     }
 }
