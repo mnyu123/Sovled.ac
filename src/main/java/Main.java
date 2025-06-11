75878489f94ea8fa956cfb97ef1f7628ca78957c
mnyu123 <whdudwo5582@gmail.com>
2025-06-11 22:11:50 +0900

    #31403 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int total = Integer.parseInt(bf.readLine()); // 영수증 총액
-        int result = 0;
-        for (int i = 0; i < 9; i++) {
-            int book = Integer.parseInt(bf.readLine()); // 9개 책가격
-            result += book;
-        }
-        System.out.println(total - result);
+        String A = bf.readLine();
+        int AA = Integer.parseInt(A);
+        String B = bf.readLine();
+        int BB = Integer.parseInt(B);
+        String C = bf.readLine();
+        int CC = Integer.parseInt(C);
+
+        System.out.println((AA + BB) - CC);
+        String plus = A + B;
+        int num1 = Integer.parseInt(plus);
+        System.out.println(num1 - CC);
     }
 }
