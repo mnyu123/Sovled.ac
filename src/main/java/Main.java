4936bb61d132d48174e927fe29f90e10a1b60984
mnyu123 <whdudwo5582@gmail.com>
2025-06-13 20:33:18 +0900

    #5554 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int hometoschool = Integer.parseInt(bf.readLine());
+        int schooltogamer = Integer.parseInt(bf.readLine());
+        int gamertostudy = Integer.parseInt(bf.readLine());
+        int studytohome = Integer.parseInt(bf.readLine());
 
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-
-        int[] arrays = new int[2];
-        int info = Integer.parseInt(st.nextToken());
-        int math = Integer.parseInt(st.nextToken());
-        int science = Integer.parseInt(st.nextToken());
-        int english = Integer.parseInt(st.nextToken());
-        arrays[0] = info + math + science + english;
-
-
-        st = new StringTokenizer(bf.readLine(), " ");
-        info = Integer.parseInt(st.nextToken());
-        math = Integer.parseInt(st.nextToken());
-        science = Integer.parseInt(st.nextToken());
-        english = Integer.parseInt(st.nextToken());
-        arrays[1] = info + math + science + english;
-
-        System.out.println(Math.max(arrays[0], arrays[1]));
+        int total = hometoschool + schooltogamer + gamertostudy + studytohome;
+        int minute = total / 60;
+        int second = total % 60;
+        System.out.println(minute);
+        System.out.println(second);
     }
 }
