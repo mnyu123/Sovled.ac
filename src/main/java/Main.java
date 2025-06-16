92ec70d651cd28b1e730b10ffb02c0e537647476
mnyu123 <whdudwo5582@gmail.com>
2025-06-16 22:51:53 +0900

    #11365 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,23 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Hello World, Judge " + (i+1) + "!");
+        while (true) {
+            String input = bf.readLine();
+            if (input.equals("END")) {
+                break;
+            }
+            System.out.println(reverse(input));
         }
+    }
+
+    public static String reverse(String A) {
+        String reverse = "";
+        String str = A;
+
+        for (int i = A.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        return reverse;
     }
 }
