393020ec707f052cc6c49e58d9b10843f1e4ef50
mnyu123 <whdudwo5582@gmail.com>
2025-06-26 21:12:31 +0900

    #33170 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,22 +6,14 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine()); // 가게수
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-            int A = Integer.parseInt(st.nextToken()); // 현위치 -> 가게까지 가는데 걸린시간
-            int B = Integer.parseInt(st.nextToken()); // 현 시점에서 빵이 들어올때까지 걸리는 시간
+        int A = Integer.parseInt(bf.readLine());
+        int B = Integer.parseInt(bf.readLine());
+        int C = Integer.parseInt(bf.readLine());
 
-            if (A <= B) {
-                min = Math.min(min, B);
-            }
-        }
-
-        if (min == Integer.MAX_VALUE) {
-            System.out.println("-1");
+        if ((A + B + C) <= 21) {
+            System.out.println("1");
         } else {
-            System.out.println(min);
+            System.out.println("0");
         }
     }
 }
