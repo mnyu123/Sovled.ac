04ce630a8046618425bbe2236fa355bd2746c93a
mnyu123 <whdudwo5582@gmail.com>
2025-06-30 21:24:05 +0900

    #25628 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,31 +6,19 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
 
-        for (int i = 0; i < 3; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-            int sh = Integer.parseInt(st.nextToken()); // 출근 시
-            int sm = Integer.parseInt(st.nextToken()); // 출근 분
-            int ss = Integer.parseInt(st.nextToken()); // 출근 초
+        int A = Integer.parseInt(st.nextToken()); // 빵 개수
+        int B = Integer.parseInt(st.nextToken()); // 패티 개수
 
-            int eh = Integer.parseInt(st.nextToken()); // 퇴근 시
-            int em = Integer.parseInt(st.nextToken()); // 퇴근 분
-            int es = Integer.parseInt(st.nextToken()); // 퇴근 초
+        int bread = A / 2; // 햄버거를 만들 수 있는 최대 빵 기준 개수
 
-            int diffS = es - ss; // 퇴근 - 출근 초
-            int diffM = em - sm; // 퇴근 - 출근 분
-            int diffH = eh - sh; // 퇴근 - 출근 시
-
-            if (diffS < 0) { // 퇴근 초가 0보다 작게나오면
-                diffS += 60; // s에다가 60더하고
-                diffM -= 1; // M은 1빼야함
-            }
-            if (diffM < 0) {
-                diffM += 60;
-                diffH -= 1;
-            }
-
-            System.out.println(diffH + " " + diffM + " " + diffS);
+        int burgers = 0;
+        if (bread <= B) {
+            burgers = bread;
+        } else {
+            burgers = B;
         }
+        System.out.println(burgers);
     }
 }
