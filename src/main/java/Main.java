98a833d889fcd83164e64235f340908ae58aa1e8
mnyu123 <whdudwo5582@gmail.com>
2025-07-12 15:10:09 +0900

    #33178 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,19 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-
-        int A = Integer.parseInt(st.nextToken()); // 빵 개수
-        int B = Integer.parseInt(st.nextToken()); // 패티 개수
-
-        int bread = A / 2; // 햄버거를 만들 수 있는 최대 빵 기준 개수
-
-        int burgers = 0;
-        if (bread <= B) {
-            burgers = bread;
-        } else {
-            burgers = B;
-        }
-        System.out.println(burgers);
+        int n = Integer.parseInt(bf.readLine());
+        System.out.println(n/10);
     }
 }
