fcab59dede90365c95a1ff27d435e05b782b4808
mnyu123 <whdudwo5582@gmail.com>
2025-08-09 01:43:32 +0900

    #11365 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,18 +6,17 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        // 꼭짓점 수 - 모서리수 + 면의 수 = 2
-        int T = Integer.parseInt(bf.readLine()); // 테스트 케이스 수
+        String text = bf.readLine();
 
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " "); // StringTokenizer 쓸때 꼭 공백이면 저거 한칸 띄웁시다
-            int V = Integer.parseInt(st.nextToken()); // 꼭짓점 수
-            int E = Integer.parseInt(st.nextToken()); // 모서리 수
+        while (!text.equals("END")) {
+            String reverse = "";
 
-            System.out.println(2 - V + E); // 면의 수 출력
+            for (int i = text.length() - 1; i >= 0; i--) {
+                reverse += text.charAt(i);
+            }
+            System.out.println(reverse);
+
+            text = bf.readLine(); // 다음줄
         }
-
-        // V - E + 구해야할 수 = 2?
-        // 구해야할 수 = 2 - V + E
     }
 }
