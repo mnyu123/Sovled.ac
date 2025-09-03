80a7078050c0efa3446dc8848fddf8dd46724386
mnyu123 <whdudwo5582@gmail.com>
2025-09-03 23:16:17 +0900

    11943번 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,17 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        // 두가지 방법이 N제곱 겹침
 
-        int result = (int) Math.pow(2, N);
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int A = Integer.parseInt(st.nextToken()); // 1
+        int B = Integer.parseInt(st.nextToken()); // 2
+
+        // 두 번째 줄 입력
+        st = new StringTokenizer(bf.readLine());
+        int C = Integer.parseInt(st.nextToken()); // 3
+        int D = Integer.parseInt(st.nextToken()); // 4
+
+        int result = Math.min(B + C, A + D);
         System.out.println(result);
     }
 }
