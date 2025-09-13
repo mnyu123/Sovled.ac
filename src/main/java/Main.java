0c502c051039c7c36a92cedfc2ee316e2e7813bc
mnyu123 <whdudwo5582@gmail.com>
2025-09-13 15:40:53 +0900

    프로그래머스 Lv0 숫자 비교하기 풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,12 @@ public class Main {
     public static class Solution {
         public int solution(int num1, int num2) {
             int answer = 0;
-            answer = num1 - num2;
+            if(num1 == num2){
+                answer = 1;
+            }
+            else{
+                answer = -1;
+            }
             return answer;
         }
     }
