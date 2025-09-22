1895fdb1a7e762853fbe1848b10397907cea79fc
mnyu123 <whdudwo5582@gmail.com>
2025-09-22 21:50:39 +0900

    13752번 문제 풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(bf.readLine());
-        int b = Integer.parseInt(bf.readLine());
+        int n = Integer.parseInt(bf.readLine());
 
-        Solution solution = new Solution();
-        System.out.println(solution.solution(a, b));
-    }
-}
+        for (int i = 0; i < n; i++) {
+            int k = Integer.parseInt(bf.readLine());
+            int count = 0;
+            for (int j = 0; j < k; j++) {
+                System.out.print("=");
+                count++;
+                if (count == k) {
+                    System.out.println("");
+                    break;
+                }
 
-class Solution {
-    public int solution(int num1, int num2) {
-        // num1 / num2를 double로 나눈 후 1000 곱하기
-        double answer = ((double) num1 / num2) * 1000;
-
-        // int로 변환해서 반환
-        return (int) answer;
+            }
+        }
     }
 }
