6f1c3f66bf94ea3238b379b7186d2fcba44dbe7a
mnyu123 <whdudwo5582@gmail.com>
2025-09-26 22:45:44 +0900

    프로그래머스 Lv1 자연수 뒤집어 배열로 만들기 풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,20 +5,31 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
 
-        for (int i = 0; i < n; i++) {
-            int k = Integer.parseInt(bf.readLine());
-            int count = 0;
-            for (int j = 0; j < k; j++) {
-                System.out.print("=");
-                count++;
-                if (count == k) {
-                    System.out.println("");
-                    break;
-                }
+        long n = Long.parseLong(bf.readLine());
 
-            }
+        Solution solution = new Solution();
+        System.out.println(solution.solution(n));
+    }
+}
+
+class Solution {
+    public int[] solution(long n) {
+
+        String n2 = String.valueOf(n);
+        //System.out.println("어떻게 나오길래? : " + n2);
+
+        int[] answer = new int[n2.length()];
+        int length = n2.length();
+        //System.out.println("길이 맞는지 : " + length); // 5 일거고 ,
+
+        for (int i = 0; i < n2.length(); i++) {
+            answer[i] = n2.charAt(length - 1) - '0';
+            System.out.print(answer[i]);
+            length--;
         }
+        // 입력이 12345가 오면
+        // 답변을 5,4,3,2,1 배열로 들어오게 해서 반환
+        return answer;
     }
 }
