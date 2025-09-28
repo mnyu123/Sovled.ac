198eb4c06053ed665f845531730bd184a1755ba2
mnyu123 <whdudwo5582@gmail.com>
2025-09-28 20:13:47 +0900

    프로그래머스 Lv1 짝수와 홀추 문제풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,30 +6,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        long n = Long.parseLong(bf.readLine());
+        int num = Integer.parseInt(bf.readLine());
 
         Solution solution = new Solution();
-        System.out.println(solution.solution(n));
+        System.out.println(solution.solution(num));
     }
 }
 
 class Solution {
-    public int[] solution(long n) {
+    public String solution(int num) {
+        String answer = "";
 
-        String n2 = String.valueOf(n);
-        //System.out.println("어떻게 나오길래? : " + n2);
-
-        int[] answer = new int[n2.length()];
-        int length = n2.length();
-        //System.out.println("길이 맞는지 : " + length); // 5 일거고 ,
-
-        for (int i = 0; i < n2.length(); i++) {
-            answer[i] = n2.charAt(length - 1) - '0';
-            System.out.print(answer[i]);
-            length--;
+        if (num % 2 == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
         }
-        // 입력이 12345가 오면
-        // 답변을 5,4,3,2,1 배열로 들어오게 해서 반환
+
         return answer;
     }
 }
