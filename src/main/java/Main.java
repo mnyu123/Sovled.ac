cd30b47f8ba999b64502dedbc0c59d3af3d6e757
mnyu123 <whdudwo5582@gmail.com>
2025-10-11 21:37:49 +0900

    34052번 문제 풀이 커밋

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] arr = {5,5};
-
-        Solution solution = new Solution();
-        System.out.println(solution.solution(arr));
-    }
-}
-
-class Solution {
-    public double solution(int[] arr) {
-        double answer = 0;
-
         int result = 0;
+        for (int i = 0; i < 4; i++) {
+            int round = Integer.parseInt(bf.readLine());
+            result += round;
 
-        for(int i=0; i<arr.length; i++){
-            result += arr[i];
         }
-        answer = (double) result / arr.length;
-
-        return answer;
+        if (result + 300 <= 1800) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
